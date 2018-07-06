b9c61f05e77967aef1f6cffcc3f6aa814bb20b03
Dongkwan Kim <todoaskit@gmail.com>
2018-07-06 18:18:45 +0900

    Add skeleton files

---
+++ WriterWrapper.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+import csv
+import datetime
+
+
+class WriterWrapper:
+
+    def __init__(self, _filename: str, _fieldnames: list):
+        file_name = (_filename + '_{0}.csv').format(datetime.datetime.now())
+        self.f = open(file_name, 'w', encoding='utf-8')
+        self.wr = csv.DictWriter(self.f, fieldnames=_fieldnames)
+        self.wr.writeheader()
+
+    def write_row(self, dct: dict):
+        self.wr.writerow(dct)
+
+    def close(self):
+        self.f.close()
+
---
+++ crawl.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+from selenium import webdriver
+from WriterWrapper import WriterWrapper
+import csv
+
+
+PATH = ''
+
+
+class URLCrawler:
+
+    def __init__(self, driver: webdriver.Chrome):
+        self.driver = driver
+
+
+class ChatCrawler:
+
+    def __init__(self, driver: webdriver.Chrome):
+        self.driver = driver
+
+
+if __name__ == '__main__':
+    pass
+
