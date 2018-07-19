bb81099cf367e6759c1b0134f10fc35a1d811a5e
Dongkwan Kim <todoaskit@gmail.com>
2018-07-19 20:02:06 +0900

    Add langid library to identify language

--- analysis/lang_analysis.py
+++ analysis/lang_analysis.py
@@ -4,7 +4,12 @@ from utill.utill import get_files_with_dir_path
 from typing import Callable
 from collections import OrderedDict
 from utill.utill import try_except, have_enough_words, is_values_of_key_matched
+
+# https://github.com/Mimino666/langdetect
 import langdetect as ld
+
+# https://github.com/saffsd/langid.py
+import langid as li
 
 
 @try_except
@@ -15,11 +20,15 @@ def detect_func(line_dict: OrderedDict, criteria_func: Callable, langdetect_func
         return ''
 
 
+def li_classify_str(s):
+    return li.classify(s)[0]
+
+
 class MultiLangChatDataLoader(MultiChatDataLoader):
 
     def __init__(self, path: str, loader_nums: int = None,
                  label_condition_func: Callable = None, label_condition_args: tuple = tuple(),
-                 words_enough: tuple=(1, 3)):
+                 words_enough: tuple = (1, 3), lang_func: Callable = ld.detect):
 
         super().__init__(
             path=path,
@@ -29,11 +38,11 @@ class MultiLangChatDataLoader(MultiChatDataLoader):
         )
 
         # Add detected language.
-        # args = (criteria_func: Callable, langdetect_func: Callable, line_key: str)
+        # args = (criteria_func: Callable, lang_func: Callable, line_key: str)
         self.add_feature('lang_author_name', detect_func,
-                         args=(have_enough_words(words_enough[0]), ld.detect, 'author_name'))
+                         args=(have_enough_words(words_enough[0]), lang_func, 'author_name'))
         self.add_feature('lang_message', detect_func,
-                         args=(have_enough_words(words_enough[1]), ld.detect, 'message'))
+                         args=(have_enough_words(words_enough[1]), lang_func, 'message'))
 
 
 if __name__ == '__main__':
@@ -42,7 +51,9 @@ if __name__ == '__main__':
     multi_lang_chat_data_loader = MultiLangChatDataLoader(
         path=description_files[0],
         label_condition_func=is_values_of_key_matched,
-        label_condition_args=({'winner': 'KOR', 'main': 'post'},),
+        label_condition_args=({'winner': 'DRAW', 'main': 'ISL'},),
+        words_enough=(1, 3),
+        lang_func=li_classify_str,
     )
 
     for lang_chat_data_loader in multi_lang_chat_data_loader:
