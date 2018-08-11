7c7cf558e0006a8bab7ad45d542bda2211871a22
Dongkwan Kim <todoaskit@gmail.com>
2018-08-11 22:24:46 +0900

    Plot lang by time

--- analysis/lang.py
+++ analysis/lang.py
@@ -1,6 +1,6 @@
 from custom_path import DATA_PATH
 from DataLoader import MultiChatDataLoader
-from utill import get_files_with_dir_path, try_except, have_enough_words
+from utill import get_files_with_dir_path, try_except, have_enough_words, iso2sec
 from WriterWrapper import WriterWrapper
 from typing import Callable, Tuple
 from collections import OrderedDict
@@ -8,6 +8,8 @@ from termcolor import cprint, colored
 from collections import Counter, defaultdict
 import os
 import pickle
+import matplotlib.pyplot as plt
+import itertools
 
 # https://github.com/Mimino666/langdetect
 import langdetect as ld
@@ -123,9 +125,80 @@ class MultiLangChatDataLoader(MultiChatDataLoader):
 
             writer = WriterWrapper(os.path.join(DATA_PATH, 'lang_dist_{}'.format(name)), fieldnames)
             for line_dict in lines:
-                print(line_dict)
                 writer.write_row(line_dict)
             writer.close()
+
+    def get_match_to_series(self):
+        match_to_series = defaultdict(lambda: defaultdict(list))
+        for data_loader in self:
+            match_tuple = (
+                data_loader.get_label('country_1'),
+                data_loader.get_label('country_2'),
+                data_loader.get_label('main'),
+                data_loader[0]['author_name'],
+            )
+            time_bin_to_lang_list = defaultdict(list)
+            for line_dict in data_loader:
+                lang_message = line_dict['lang_message']
+                time_stamp_sec = iso2sec(line_dict['time_stamp'])
+                if lang_message:
+                    time_bin_to_lang_list[int(time_stamp_sec % 60)].append(lang_message)
+            total_lang_list = list(set(itertools.chain(*time_bin_to_lang_list.values())))
+            time_bin_to_lang_counter = {
+                time_bin: Counter(lang_list) for time_bin, lang_list in time_bin_to_lang_list.items()
+            }
+            for time_bin, lang_counter in sorted(time_bin_to_lang_counter.items(), key=lambda x: x[0]):
+                match_to_series[match_tuple]['time_bin'].append(time_bin)
+                for lang in total_lang_list:
+                    match_to_series[match_tuple][lang].append(lang_counter[lang] if lang in lang_counter else 0)
+        return match_to_series
+
+    def plot_match_to_series(self, options=None):
+        # https://matplotlib.org/users/pyplot_tutorial.html
+        options = options if options else {}
+        num_of_plots = None if 'num_of_plots' not in options else options['num_of_plots']
+        with_en = True if 'with_en' not in options else options['with_en']
+        show_or_save = 'show' if 'show_or_save' not in options else options['show_or_save']
+
+        match_to_series = self.get_match_to_series()
+        for match, series in match_to_series.items():
+
+            plt_kwargs_list = []
+            for label, values in sorted(series.items(), key=lambda kv: -sum(kv[1])):
+                if label == 'time_bin':
+                    continue
+                plt_kwargs_list.append({
+                    'x': series['time_bin'],
+                    'y': values,
+                    'label': label,
+                })
+
+            if not with_en:
+                plt_kwargs_list = [kw for kw in plt_kwargs_list if kw['label'] != 'en']
+
+            if num_of_plots:
+                plt_kwargs_list = plt_kwargs_list[:num_of_plots]
+
+            plots = []
+            for plt_kwargs in plt_kwargs_list:
+                _plot = plt.plot(plt_kwargs['x'], plt_kwargs['y'], label=plt_kwargs['label'])
+                plots.append(_plot[0])
+
+            plt.xlabel('min')
+            plt.title('_'.join(match[:-1]))
+            plt.legend(handles=plots)
+
+            if show_or_save == 'show':
+                plt.show()
+            else:
+                plots_path = '../data/plots/{}'.format(self.info)
+                if not os.path.isdir(plots_path):
+                    os.mkdir(plots_path)
+                plt.savefig('{}/{}.png'.format(plots_path, '_'.join(match[:-1])))
+
+            plt.clf()
+            plt.cla()
+            plt.close()
 
 
 if __name__ == '__main__':
@@ -139,12 +212,15 @@ if __name__ == '__main__':
         lang_func=ld.detect,
     )
 
-    for lang_chat_data_loader in multi_lang_chat_data_loader[{}]:
-        for line in lang_chat_data_loader.lines:
-            print({k: v for k, v in line.items() if k != 'img'})
-        print(lang_chat_data_loader.label_dict)
-
-    multi_lang_chat_data_loader.export_stats()
-
     if multi_lang_chat_data_loader.is_dump_possible():
         multi_lang_chat_data_loader.dump()
+
+    MODE = 'PLOT'
+    if MODE == 'PLOT':
+        multi_lang_chat_data_loader.plot_match_to_series({
+            'num_of_plots': 3,
+            'with_en': True,
+            'show_or_save': 'save',
+        })
+    elif MODE == 'STATS':
+        multi_lang_chat_data_loader.export_stats()
